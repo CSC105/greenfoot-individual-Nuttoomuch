e65edfc33aefab62755bbcd8138aa1e8a42c41fa
Nuttoomuch <36223147+Nuttoomuch@users.noreply.github.com>
2018-03-03 18:31:26 +0700

    put author name

--- GravityFlamingo/Start.java
+++ GravityFlamingo/Start.java
@@ -3,8 +3,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class Start here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Prapatsorn Ouisakul
+ * @version 1
  */
 public class Start extends World
 {
